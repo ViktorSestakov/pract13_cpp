d32555ab97860bfc5b84ba2d1b865878fe7d6e34
BNKTOP IIIecTakoB <viktorsestakov431@gmail.com>
2026-03-19 09:50:45 +0500

    Update pract13_cpp.cpp

--- pract13_cpp.cpp
+++ pract13_cpp.cpp
@@ -1,12 +1,16 @@
-﻿#include <iostream>
+#include <iostream>
 #include <windows.h>
 
 using namespace std;
 
-string names[10] {"Иван", "Роман", "Пётр", "Николай", "Олег", "Александр", "Тимофей", "Гога", "Игнат", "Антон"};
+string names[10]{ "Иван", "Роман", "Пётр", "Николай", "Олег", "Александр", "Тимофей", "Гога", "Игнат", "Антон" };
 
 HANDLE pThread[10];
 HANDLE bThread;
+
+const char* PlayerEvent = "PlayerEvent";
+
+HANDLE pEvent = CreateEventA(NULL, FALSE, FALSE, PlayerEvent);
 
 struct Bayum {
 	long health = 9000000000;	// здоровье босса
@@ -28,25 +32,82 @@ struct Player {
 	char name[64];				// имя игрока
 };
 
-void PlayersPlay() {
-	cout << "123" << endl;
+struct GamePlayPlayer
+{
+	Player pl;
+	int colvoPlayers;
+};
+
+Player players[10];
+Bayum boss;
+
+void Lose() {
+
+}
+
+void Win() {
+
+}
+
+void GamePlay(GamePlayPlayer gpl) {
+	int attackPlayer = gpl.pl.damage * (100 - boss.resist) / 100;
+	int spAttackPlayer = gpl.pl.specialDamage * (1 - (boss.resist * 0.01));
+	
+	int attackBoss = boss.damage * (1 - (gpl.pl.defense * 0.01));
+	int spAttackBoss = boss.specialDamage * 0.05 * (gpl.colvoPlayers - 1);
+	
+	// обычная атака игрока
+	if (gpl.pl.health > 0) {
+		boss.health -= attackPlayer;
+		Sleep(gpl.pl.attckCooldown * 1000);
+	}
+
+	// спец атака игрока
+	if (gpl.pl.health > 0) {
+		boss.health -= spAttackPlayer;
+		Sleep(gpl.pl.specialCooldown * 1000);
+	}
+
+	// обычная атака босса
+	if (boss.health > 0) {
+		gpl.pl.health -= attackBoss;
+		Sleep(boss.attackCooldown * 1000);
+	}
+
+	// спец атак босса
+	if (boss.health > 0) {
+		if (gpl.colvoPlayers > 1) {
+			gpl.pl.health -= spAttackBoss;
+		}
+		else {
+			gpl.pl.health -= boss.specialDamage;
+		}
+		Sleep(boss.specialCooldown * 1000);
+	}
 }
 
 void CreateThreadPlayer(int colvo) {
 	for (int i = 0; i < colvo; i++)
 	{
-		pThread[i] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)PlayersPlay, NULL, 0, 0);
+		GamePlayPlayer player;
+		player.colvoPlayers = colvo;
+
+		strcpy_s(players[i].name, names[i].c_str());
+		player.pl = players[i];
+
+		pThread[i] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)GamePlay, &player, 0, 0);
 
 		if (pThread[i] == NULL)
 		{
 			cout << "Не удалось создать поток!" << endl;
 			return;
 		}
+
+		if (pEvent == NULL) {
+			cout << "Не удалось создать поток!" << endl;
+			return;
+		}
 	}
-}
-
-void BossPlay() {
-
 }
 
 int main()
@@ -65,6 +126,8 @@ int main()
 	else {
 		cout << "Не верно выбрано кол-во человек!" << endl;
 	}
+
+	Sleep(5000);
 
 	return 0;
 }
